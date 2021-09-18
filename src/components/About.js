19e9ad7774aeca896fc4198b810ed977d13273e5
ayazmahmood10 <ayazmahmood0015@gmail.com>
2021-09-18 22:32:43 +0530

    Fixed the about component issues

--- src/components/About.js
+++ src/components/About.js
@@ -19,36 +19,37 @@ const About = (props) => {
                         <strong>Analyze Your Text</strong>
                     </button>
                 </h2>
-                <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+                <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
                     <div className="accordion-body" style={myStyle}>
-                    TextTract gives you a way to analyze your text quickly and efficiently. Be it word count, character count, case conversion of your text, removal of spaces and special characters and many more.
+                    <em>TextTract gives you a way to analyze your text quickly and efficiently. Be it word count, character count, case conversion of your text, removal of spaces and special characters and many more.</em>
                     </div>
                 </div>
             </div>
             <div className="accordion-item" style={myStyle}>
                 <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
-                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    <button className="accordion-button collapsed"  type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
                         <strong>Usage is totally free!</strong>
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                     <div className="accordion-body" style={myStyle}>
-                    TextTract is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    <em>TextTract is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.</em>
                     </div>
                 </div>
             </div>
             <div className="accordion-item" style={myStyle}>
                 <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    <button className="accordion-button collapsed" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                         <strong>Compatibility</strong>
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                     <div className="accordion-body" style={myStyle}>
-                    This utility software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera
+                    <em>This utility software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera</em>
                     </div>
                 </div>
             </div>
+            
         </div>
       </div>  
     );
